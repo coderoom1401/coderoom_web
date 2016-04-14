d834ef76ef993f8ccfb75fbc008321f73c2021bc
jingyuan.wang <jingyuan.wang@ele.me>
2016-04-14 17:12:07 +0800

    加的header瞧瞧

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-
-	// 发送响应数据 "Hello World"
-	response.end('Hello World\n');
-}).listen(18080);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var server = connect();
+server.use(serveStatic('/output/index.html'));
+server.listen(18080);
